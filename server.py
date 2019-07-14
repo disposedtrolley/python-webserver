a1e9ccdf658ec734705ca946c77450679bbf193f
James Liu <james_liu@fastmail.com>
2019-07-14 16:30:30 +1000

    feat: add concurrency

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import os
 
 SERVER_ADDRESS = (HOST, PORT) = '', 8888
 REQUEST_QUEUE_SIZE = 5
@@ -7,6 +8,11 @@ REQUEST_QUEUE_SIZE = 5
 
 def handle_request(client_connection):
     request_data = client_connection.recv(1024)
+    print(
+        "Child PID: {pid}. Parent PID: {ppid}".format(
+            pid=os.getpid(),
+            ppid=os.getppid(),
+        ))
     print(request_data.decode("utf-8"))
 
     http_response = b"""\
@@ -23,12 +29,19 @@ def serve_forever():
     listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     listen_socket.bind(SERVER_ADDRESS)
     listen_socket.listen(REQUEST_QUEUE_SIZE)
-    print(f"Serving HTTP on port {PORT} ...")
+    print("Serving HTTP on port {port} ...".format(port=PORT))
+    print("Parent PID (PPID): {pid}\n".format(pid=os.getpid()))
 
     while True:
         client_connection, client_address = listen_socket.accept()
-        handle_request(client_connection)
-        client_connection.close()
+        pid = os.fork()
+        if pid == 0:    # child
+            listen_socket.close()   # close child copy
+            handle_request(client_connection)
+            client_connection.close()
+            os._exit(0)     # child exits here
+        else:
+            client_connection.close()   # close parent copy
 
 
 if __name__ == "__main__":
