d97b63aac2651d209f09938dee75736e038003b5
James Liu <james_liu@fastmail.com>
2019-07-14 11:39:05 +1000

    refactor: organise into functions

--- main.py
+++ main.py
@@ -1,15 +1,9 @@
 import socket
 
-HOST, PORT = '', 8888
+SERVER_ADDRESS = (HOST, PORT) = '', 8888
+REQUEST_QUEUE_SIZE = 5
 
-listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
-listen_socket.bind((HOST, PORT))
-listen_socket.listen(1)
-print(f"Serving HTTP on port {PORT} ...")
-
-while True:
-    client_connection, client_address = listen_socket.accept()
+def handle_request(client_connection):
     request_data = client_connection.recv(1024)
     print(request_data.decode("utf-8"))
 
@@ -18,6 +12,19 @@ HTTP/1.1 200 OK
 
 Hello, World!
 """
-
     client_connection.sendall(http_response)
-    client_connection.close()
+
+def serve_forever():
+    listen_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    listen_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    listen_socket.bind((HOST, PORT))
+    listen_socket.listen(1)
+    print(f"Serving HTTP on port {PORT} ...")
+
+    while True:
+        client_connection, client_address = listen_socket.accept()
+        handle_request(client_connection)
+        client_connection.close()
+
+if __name__ == "__main__":
+    serve_forever()
